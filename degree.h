a82186adab1551d5faf0d934339e69b185215e45
Zarinari <79060489+Zarinari@users.noreply.github.com>
2021-02-14 14:57:25 -0600

    Create degree.h
    Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
     
    Note: This information should be included in the degree.h file.

---
+++ degree.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+enum DegreeProgram {SECURITY, NETWORK, SOFTWARE, OTHER};
+
+static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE", "OTHER" };
