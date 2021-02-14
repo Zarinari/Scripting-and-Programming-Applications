6953f161b9c25ecd95a1d7a0f3adee9c88eb8810
Zarinari <79060489+Zarinari@users.noreply.github.com>
2021-02-14 15:23:47 -0600

    Create student.h
    D.  For the Student class, do the following:
    1.  Create the class Student  in the files student.h and student.cpp, which includes each of the following variables:
    •  student ID
    •  first name
    •   last name
    •  email address
    •  age
    •  array of number of days to complete each course
    •  degree program
    
    2.  Create each of the following functions in the Student class:
    a.  an accessor (i.e., getter) for each instance variable from part D1
    b.  a mutator (i.e., setter) for each instance variable from part D1
    c.  All external access and changes to any instance variables of the Student class must be done using accessor and mutator functions.
    d.  constructor using all of the input parameters provided in the table
    e.  print() to print specific student data

---
+++ student.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "degree.h"
+
+using std::cout;
+using std::string;
+
+class Student {
+public:
+    const static int daysArraySize = 3;
+private:
+    string studentID;
+    string firstName;
+    string lastName;
+    string emailAddress;
+    int studentAge;
+    int daysInCourse[daysArraySize];
+    DegreeProgram degreeProgram;
+public:
+    Student();
+    Student(string studentID, string firstName, string lastName, string emailAddress, int studentAge, int daysInCourse[], DegreeProgram degreeProgram);
+    ~Student();
+    
+    //accessors
+    string getID();
+    string getFirst();
+    string getLast();
+    string getEmail();
+    int getAge();
+    int* getDays();
+    DegreeProgram getDegree();
+    
+    //mutators
+    void setID(string ID);
+    void setFirst(string first);
+    void setLast(string last);
+    void setEmail(string email);
+    void setAge(int age);
+    void setDays(int days[]);
+    void setDegree(DegreeProgram dp);
+    
+    static void printHeader();
+    
+    void print();
+    
+    };
