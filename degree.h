d2beb19816bbc2257dea1d59c542969d26c42267
Zarinari <79060489+Zarinari@users.noreply.github.com>
2021-02-15 13:49:00 -0600

    Update degree.h

--- degree.h
+++ degree.h
@@ -2,4 +2,4 @@
 #include <string>
 enum DegreeProgram {SECURITY, NETWORK, SOFTWARE, OTHER};
 
-static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE", "OTHER" };
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE", "OTHER" };
