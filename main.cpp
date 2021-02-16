18aed643449555b81619a344ece8cd49ee5ffd79
Zarinari <79060489+Zarinari@users.noreply.github.com>
2021-02-15 23:07:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     cout << "Scripting and Programming - Applications - C867 | C++ | #001220083 | Hannah Sullivant" << std::endl << std::endl;
     
     const string studentData[] = {
-        "A1,John,Smith,John1989@gmail.com,20,30,35,40,SECURITY",
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
